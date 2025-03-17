da9a30a413437fa292636baf4a458b0d9ded011e
Janaka Madusanka <rmjmrathnayaka@std.appsc.sab.ac.lk>
2025-03-17 18:52:03 +0530

    Login controller done and tested.

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -47,9 +47,34 @@ export const signup = async (req, res) => {
     }
 
 }
-export const login = (req, res) => {
-    res.send(" Login route");
+
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email })
+        if (!user) return res.status(400).json({ message: "Invalid credentials" });
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+
+        //generate jwt token
+        generateToken(user._id, res);
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+
+    } catch (error) {
+        console.log("Error in login", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+
+    }
 }
-export const logout = (req, res) => {
-    res.send("Logout route");
+
+
+
+export const logout = async (req, res) => {
+
 }
