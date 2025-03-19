0e73f779887ee78217b61db66dd553ecf981a6f5
Janaka Madusanka <rmjmrathnayaka@std.appsc.sab.ac.lk>
2025-03-20 00:07:00 +0530

    Basic pages and routs are setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import SettingPage from './pages/SettingPage'
+import ProfilePage from './pages/ProfilePage'
+import Navbar from './components/Navbar'
+
 
 const App = () => {
   return (
-    <div className=
-      "text-red-500">
-      App
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/setting" element={<SettingPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
 
+
+      </Routes>
     </div>
   )
 }
---
+++ Frontend/src/pages/SettingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SettingPage = () => {
+    return (
+        <div>
+            <h1>Setting page</h1>
+        </div>
+    )
+}
+
+export default SettingPage
