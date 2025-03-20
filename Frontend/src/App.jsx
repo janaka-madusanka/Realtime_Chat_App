525160fbc9abeb7b8050f1388c8a833dbe3ead74
Janaka Madusanka <rmjmrathnayaka@std.appsc.sab.ac.lk>
2025-03-20 23:34:11 +0530

    Routs protected logic added.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
   }, [checkAuth]);
   console.log({ authUser });
 
-  if (true) return (
+  if (isCheckingAuth && !authUser) return (
     <div className="flex items-center justify-center h-screen">
       <Loader className="size-10 animate-spin" />
 
@@ -34,11 +34,11 @@ const App = () => {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={authUser ? <SignUpPage /> : <Navigate to="/login" />} />
+        <Route path="/login" element={authUser ? <LoginPage /> : <Navigate to="/login" />} />
         <Route path="/setting" element={<SettingPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
 
 
       </Routes>
