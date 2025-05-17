d7bd2c9ef0a3536b013a68eb2ee2de5cbe1bc2f0
Janaka Madusanka <160509773+janaka-madusanka@users.noreply.github.com>
2025-05-17 15:43:26 +0530

    Profile page done and fix env file typo error.

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -97,7 +97,7 @@ export const updateProfile = async (req, res) => {
         if (!profilePic) {
             return res.status(400).json({ message: "Profile pic is required" });
         }
-        const uploadResponse = await cloudinary.uploader.upload(updatePic)
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
         const updatedUser = await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true });
 
         res.status(200).json(updatedUser);
